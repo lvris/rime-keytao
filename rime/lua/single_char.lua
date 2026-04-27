3e534b145a90c8b254194acfa59343604d0ef8f7
lvris <lvris@qq.com>
2026-04-28 02:52:53 +0300

    🔨防止单字顶掉词

--- rime/lua/single_char.lua
+++ rime/lua/single_char.lua
@@ -1,19 +1,23 @@
 --[[
-single_char_filter: 候选项重排序，使单字优先
+single_char_filter: 候选项重排序，使短注释/单字优先
 --]]
 
-local function filter(input)
-   local l = {}
-   for cand in input:iter() do
-      if (utf8.len(cand.text) == 1) then
-	 yield(cand)
-      else
-	 table.insert(l, cand)
-      end
-   end
-   for i, cand in ipairs(l) do
-      yield(cand)
-   end
+local function single_char_filter(input)
+    local l = {}
+    for cand in input:iter() do
+        if cand.comment and cand.comment:len() == 0 then
+            yield(cand)
+        else
+            if (utf8.len(cand.text) == 1) then
+                yield(cand)
+            else
+                table.insert(l, cand)
+            end
+        end
+    end
+    for i, cand in ipairs(l) do
+        yield(cand)
+    end
 end
 
-return filter
+return single_char_filter
